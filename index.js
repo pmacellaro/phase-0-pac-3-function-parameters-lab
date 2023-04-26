094a897e333815eccb82c10834dfef2a822da598
Paul <pmacellaro7@aol.com>
2023-04-26 14:54:20 -0400

    ayeo

---
+++ index.js
@@ -0,0 +1,11 @@
+function logTwoValues(Four, value2) {
+    console.log(`The two values are ${Four} and ${value2}.`);}
+logTwoValues( four,five)
+logTwoValues(Four = "seven", value2)
+function introductionWithLanguageOptional(name,language=JavaScript) 
+{console.log(`I am ${name} really trying to somehow grasp ${language}`)}
+
+  
+  
+
+
